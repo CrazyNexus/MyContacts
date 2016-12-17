080e151f3cb952bf2ca89cc6bfd5256de5cbefe2
Thomas Dubiel <td@crazynexus.de>
2016-12-17 20:29:24 +0100

    zwischendurchcommit

---
+++ MyContacts.iOS/ContactListTableViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace MyContacts.iOS
+{
+    public partial class ContactListTableViewController : UITableViewController
+    {
+        public ContactListTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
